d87e94fc3796f6dc9cd0493385f813c999b84242
BarnoMamadzhanova <b@mamadzhanova.com>
2023-11-22 11:03:05 +0100

    first commit

---
+++ exercises.php
@@ -0,0 +1,15 @@
+
+<!-- PHP | Day 7 | Classwork -->
+
+
+Create a simple Class called Vehicles. This Class should have the properties: name, model, makeYear, color, fuelType.
+Use a constructor. Create a method which will return the name and the model of this vehicle. 
+
+
+
+<!-- Instantiate 3 new objects from this Class. 
+
+Once you have done creating these objects, create a new Class called Ships. This Class will extend the Vehicles Class. 
+Add new properties and methods to this Class.
+
+Instantiate 3 new objects from this Class.  -->
